a9b9ef9421ac57fbce9696d74188d3422cd88033
Michael B <newmdb1710@Michaels-MacBook-Pro.local>
2024-01-22 11:24:45 -0500

    added header & navbar

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return(
+        <div>
+            <h1 className="text-white">This is the temp navbar</h1>
+        </div>
+    )
+}
+
+export default NavBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import Image from 'next/image'
 
+/* Components */
+
+// import NavBar from './components/NavBar'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+     
+      </div>
       <h1>90s Teen Culture coming soon</h1>
       <p>Movies, Music, TV, Tech</p>
     </main>
