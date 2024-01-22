938b3032fa2a02e618910a5bbc0846ada032d417
Michael B <newmdb1710@Michaels-MacBook-Pro.local>
2024-01-22 10:31:05 -0500

    test line to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>90s Teen Culture coming soon</h1>
+      <p>Movies, Music, TV, Tech</p>
     </main>
   )
 }
